65e426ac1b2e10b422d70573bc9a2cdaa1005a1c
ferbemar <ferbemar@students.zhaw.ch>
2022-10-05 18:53:28 +0200

    Create Budget.cs
    first draft
    group has been ignored.
    no Budget seems to be at the wrong spot here.

---
+++ AcademicTimePlanner/DataMapping/Budget/Budget.cs
@@ -0,0 +1,52 @@
+﻿using AcademicTimePlanner.DataMapping.Plan;
+
+namespace AcademicTimePlanner.DataMapping.Budget
+{
+    public class Budget
+    {
+        private Guid _id;
+        private string _name;
+        private int _duration;
+        private bool _noBudget; //why does this exist? would this not be the name???
+        private LinkedList<PlanProjectBudget> _planProjectBudgets;
+
+        public Budget(string name, int duration) 
+        { 
+            _id = Guid.NewGuid();
+            _name = name;
+            _duration = duration;
+            _noBudget = false;
+            _planProjectBudgets = new LinkedList<PlanProjectBudget>();
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public string Name { 
+            get { return _name; } 
+        }
+
+        public int Duration
+        {
+            get { return _duration; }
+        }
+
+        public bool NoBudget
+        {
+            get { return _noBudget; }   
+        }
+
+        public void AddBudget(PlanProjectBudget planProjectBudget)
+        {
+            _planProjectBudgets.AddLast(planProjectBudget);
+        }
+
+        public void RemoveBudget(PlanProjectBudget planProjectBudget)
+        {
+            _planProjectBudgets.Remove(planProjectBudget);
+        }
+
+    }
+}
