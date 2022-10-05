439cb25d8bf597fd6f2d94c81baddd455ad7706c
ferbemar <ferbemar@students.zhaw.ch>
2022-10-05 16:04:09 +0200

    first draft toggl
    add a method to set the projectID for a toggl task.

---
+++ AcademicTimePlanner/DataMapping/Toggl/TogglEntrySum.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademicTimePlanner.DataMapping.Toggl
+{
+    public class TogglEntrySum
+    {
+        private DateOnly _date;
+        private int _duration;
+
+        public DateOnly Date { get { return _date; } set { _date = value; } }
+        public int Duration { get { return _duration; } set { _duration = value; } }
+    }
+}
---
+++ AcademicTimePlanner/DataMapping/Toggl/TogglTask.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademicTimePlanner.DataMapping.Toggl
+{
+    public class TogglTask
+    {
+        private Guid _id;
+        private int _togglId;
+        private string _name;
+        private Guid _projectId;
+
+        public Guid Id { get { return _id; } set { _id = value; } }
+
+        public int TogglId { get { return _togglId; } set { _togglId = value; } }
+
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public Guid ProjectId { get { return _projectId; } set { _projectId = value; } }
+    }
+}
