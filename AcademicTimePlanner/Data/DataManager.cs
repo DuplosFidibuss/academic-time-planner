abb3a67397069a7894c8755ea027b1e922bc4167
DuplosFidibuss <lukas.jans@swisscows.email>
2022-10-10 21:47:46 +0200

    Create DataManager.cs

---
+++ AcademicTimePlanner/Data/DataManager.cs
@@ -0,0 +1,25 @@
+﻿using AcademicTimePlanner.DataMapping.Budget;
+using AcademicTimePlanner.DataMapping.Plan;
+using AcademicTimePlanner.DataMapping.Toggl;
+
+namespace AcademicTimePlanner.Data
+{
+    public class DataManager
+    {
+        private DataManager()
+        {
+            Budgets = new LinkedList<Budget>();
+            PlanProjects = new LinkedList<PlanProject>();
+            TogglProjects = new LinkedList<TogglProject>();
+        }
+
+        /// <summary>
+        /// This class holds all data used by the application.
+        /// </summary>
+        public static readonly DataManager Instance = new();
+
+        public LinkedList<Budget> Budgets { get; }
+        public LinkedList<PlanProject> PlanProjects { get; }
+        public LinkedList<TogglProject> TogglProjects { get; }
+    }
+}
