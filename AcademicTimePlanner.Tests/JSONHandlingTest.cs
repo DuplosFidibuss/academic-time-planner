e272c9e05dfc0ee64c2fb03f2fcb4561070cd652
ferbemar <ferbemar@students.zhaw.ch>
2022-10-11 19:11:43 +0200

    Create JSONHandlingTest.cs
    first draft of test.
    does not work as it does not compile.

---
+++ AcademicTimePlanner.Tests/JSONHandlingTest.cs
@@ -0,0 +1,37 @@
+﻿using AcademicTimePlanner.DataMapping.Plan;
+using AcademicTimePlanner.JSONHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicTimePlanner.Tests
+{
+    [TestClass]
+    internal class JSONHandlingTest
+    {
+        private DateOnly startDate = new DateOnly(2022, 1, 1);
+        private DateOnly endDate = new DateOnly(2022, 1, 2);
+        private PlanEntry testPlanEntry;
+        private PlanTask testPlanTask;
+        private PlanProject testPlanProject;
+        private importExportJSON ieJson = new importExportJSON();
+
+        [TestInitialize]
+        public void initialize() 
+        {
+            testPlanEntry = new PlanEntry("testPlanEntry_1", startDate, endDate, 60000);
+            testPlanTask = new PlanTask(1,"testTask_1");
+            testPlanTask.AddPlanEntry(testPlanEntry);
+            testPlanProject = new PlanProject(1, "testProject_1");
+            testPlanProject.AddPlanTask(testPlanTask);
+        }
+
+        [TestMethod]
+        public void testSafeJson()
+        {
+            ieJson.safeJson(testPlanProject);
+        }
+    }
+}
