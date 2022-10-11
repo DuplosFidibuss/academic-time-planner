ffc00d8c60e071c6125b1c8d617c2570bd156205
ferbemar <ferbemar@students.zhaw.ch>
2022-10-11 20:06:15 +0200

    Update importExportJSON.cs
    path is now a constant. doesn't work as of now

--- AcademicTimePlanner/JSONHandling/importExportJSON.cs
+++ AcademicTimePlanner/JSONHandling/importExportJSON.cs
@@ -7,6 +7,8 @@ namespace AcademicTimePlanner.JSONHandling
 {
     public class importExportJSON
     {
+        private const string DataPath = @"..\..\AcademicTimePlanner.Tests\JSON_Files\ATP_data.json";
+
         JsonSerializerOptions options = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,  // set camelCase       
@@ -18,17 +20,16 @@ namespace AcademicTimePlanner.JSONHandling
         // pass options to deserializer
         //var order = JsonSerializer.Deserialize<ModelClass>(json, options);
 
-        public PlanProject loadJson() {
+        /*public PlanProject loadJson() {
             string fileName = "PlanProject.json";
             string jsonString = File.ReadAllText(fileName);
             return JsonSerializer.Deserialize<PlanProject>(jsonString);
         }
-
+        */
         public void safeJson(PlanProject project)
         {
             string jsonString = JsonSerializer.Serialize(project, options);
-            StreamWriter outputFile = new StreamWriter("data.json");
-            outputFile.WriteLine(jsonString);
+            File.WriteAllText(DataPath, jsonString);
         }
     }
 }
