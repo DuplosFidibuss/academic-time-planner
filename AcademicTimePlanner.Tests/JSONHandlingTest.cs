31efecb788db165b2bb6d48fdaec68e540d8a8b2
ferbemar <ferbemar@students.zhaw.ch>
2022-10-12 16:31:57 +0200

    Update JSONHandlingTest.cs
    test now also includes a PlanEntryRepetition.
    (does not seem to work as intended)

--- AcademicTimePlanner.Tests/JSONHandlingTest.cs
+++ AcademicTimePlanner.Tests/JSONHandlingTest.cs
@@ -11,9 +11,10 @@ namespace AcademicTimePlanner.Tests
     [TestClass]
     public class JSONHandlingTest
     {
-        private DateOnly startDate = new DateOnly(2022, 1, 1);
-        private DateOnly endDate = new DateOnly(2022, 1, 2);
+        private DateTime startDate = new DateTime(2022, 1, 1);
+        private DateTime endDate = new DateTime(2022, 1, 2);
         private PlanEntry testPlanEntry;
+        private PlanEntryRepetition testPlanEntryRepetition;
         private PlanTask testPlanTask;
         private PlanProject testPlanProject;
         private importExportJSON ieJson = new importExportJSON();
@@ -23,8 +24,11 @@ namespace AcademicTimePlanner.Tests
         {
             testPlanEntry = new PlanEntry("testPlanEntry_1", startDate, endDate, 60000);
 
+            testPlanEntryRepetition = new PlanEntryRepetition("testPlanEntryRepetition_1", startDate, endDate, 600000, 60000);
+
             testPlanTask = new PlanTask(1,"testTask_1");
             testPlanTask.AddPlanEntry(testPlanEntry);
+            testPlanTask.AddRepetitionEntry(testPlanEntryRepetition);
 
             testPlanProject = new PlanProject(1, "testProject_1");
             testPlanProject.AddPlanTask(testPlanTask);
@@ -35,12 +39,15 @@ namespace AcademicTimePlanner.Tests
         {
             ieJson.saveJson(testPlanProject);
         }
-
+        
         [TestMethod]
         public void testLoadJson()
         {
             string directory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName; ;
             string dataPath = directory + @"\AcademicTimePlanner.Tests\JSON_Files\PlanProject.json";
+            PlanProject loadedPlanProject = ieJson.loadJson(dataPath);
+            Assert.AreEqual(testPlanProject.Name, loadedPlanProject.Name);
         }
+        
     }
 }
