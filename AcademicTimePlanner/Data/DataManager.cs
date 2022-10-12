6dbd0c7d8c7d67a798c5527e893d3a19f82d0bc5
DuplosFidibuss <lukas.jans@swisscows.email>
2022-10-12 10:10:59 +0200

    Remove singleton instance form DataManager.

--- AcademicTimePlanner/Data/DataManager.cs
+++ AcademicTimePlanner/Data/DataManager.cs
@@ -6,17 +6,15 @@ namespace AcademicTimePlanner.Data
 {
     public class DataManager
     {
-        private DataManager()
+        /// <summary>
+        /// This class holds all data used and created by the application.
+        /// </summary>
+        public DataManager()
         {
             Budgets = new LinkedList<Budget>();
             PlanProjects = new LinkedList<PlanProject>();
             TogglProjects = new LinkedList<TogglProject>();
         }
-
-        /// <summary>
-        /// This class holds all data used by the application.
-        /// </summary>
-        public static readonly DataManager Instance = new();
 
         public LinkedList<Budget> Budgets { get; }
         public LinkedList<PlanProject> PlanProjects { get; }
