afb32d674f146b49587e76227a2a26ce4860fefd
DuplosFidibuss <lukas.jans@swisscows.email>
2022-10-13 12:04:13 +0200

    Add interface documentation for related interfaces.

--- AcademicTimePlanner/Services/DataManagerService/IDataManagerService.cs
+++ AcademicTimePlanner/Services/DataManagerService/IDataManagerService.cs
@@ -1,9 +1,19 @@
-﻿using AcademicTimePlanner.DataMapping.Toggl;
+﻿using AcademicTimePlanner.Data;
+using AcademicTimePlanner.DataMapping.Toggl;
 
 namespace AcademicTimePlanner.Services.DataManagerService
 {
+    /// <summary>
+    /// Handles access to the <see cref="DataManager"/> of the current session.
+    /// The <see cref="DataManager"/> can only be accessed via this service.
+    /// </summary>
     public interface IDataManagerService
     {
+        /// <summary>
+        /// Updates the Toggl data hold by <see cref="DataManager"/>.
+        /// </summary>
+        /// <param name="togglProjects"></param>
+        /// <returns><c>true</c> if the update was successful, otherwise <c>false</c>.</returns>
         public Task<bool> SetTogglProjects(List<TogglProject> togglProjects);
     }
 }
--- AcademicTimePlanner/Services/TogglService/ITogglService.cs
+++ AcademicTimePlanner/Services/TogglService/ITogglService.cs
@@ -2,7 +2,16 @@ using AcademicTimePlanner.DataMapping.Toggl;
 
 namespace AcademicTimePlanner.Services.TogglService;
 
+/// <summary>
+/// Proceeds Toggl raw data to ATP-conform Toggl data.
+/// </summary>
 public interface ITogglService
 {
+    /// <summary>
+    /// Converts all Toggl raw entries from the given date until calling time to
+    /// <see cref="TogglEntrySum"/>s and groups them into <see cref="TogglProject"/>s.
+    /// </summary>
+    /// <param name="since"></param>
+    /// <returns>A list containing the created<see cref="TogglProject"/>s</returns>
     Task<List<TogglProject>> GetTogglProjects(DateOnly since);
 }
