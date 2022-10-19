ff3ac597f14263fd8206f5c1272eb6d820c4cd2a
DuplosFidibuss <lukas.jans@swisscows.email>
2022-10-19 13:49:03 +0200

    Create ChartData.cs

---
+++ AcademicTimePlanner/Data/ChartData.cs
@@ -0,0 +1,24 @@
+﻿using AcademicTimePlanner.DataMapping.Plan;
+using AcademicTimePlanner.DataMapping.Toggl;
+
+namespace AcademicTimePlanner.Data
+{
+    public class ChartData
+    {
+        public ChartData(List<TogglProject> togglProjects, List<PlanProject> planProjects)
+        {
+            TogglProjects = togglProjects;
+            PlanProjects = planProjects;
+        }
+
+        public List<TogglProject> TogglProjects { get; set; }
+
+        public List<PlanProject> PlanProjects { get; set; }
+
+        public int TotalTrackedTime { get; set; }
+
+        public int TotalPlannedTime { get; set; }
+
+        public int RemainingTime { get; set; }
+    }
+}
