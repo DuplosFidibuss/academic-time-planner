8c04fcde009e1b72675b3d7979ee4fcbc442a8f7
ferbemar <ferbemar@students.zhaw.ch>
2022-11-09 11:56:34 +0100

    Update TogglProject.cs
    EndDate of entry is now midnight of the end of the day.
    Now correctly displays a line if the data points  are out of range of the start and/or end date

--- AcademicTimePlanner/DataMapping/Toggl/TogglProject.cs
+++ AcademicTimePlanner/DataMapping/Toggl/TogglProject.cs
@@ -64,8 +64,25 @@
             var durationsPerDateInTimeRange = new SortedDictionary<DateTime, double>();
             var durationsPerDate = GetDurationsPerDate();
             var dates = durationsPerDate.Keys.ToList();
+            var startDateEntry = DateTime.MinValue;
+            var endDateEntry = DateTime.MaxValue;
 
-            for(int i = 0; i < durationsPerDate.Count; i++)
+            for (int i = 0; i < durationsPerDate.Count; i++)
+            {
+                var date = dates[i];
+                if (date > startDate && startDateEntry == DateTime.MinValue)
+                {
+                    if (i > 0)
+                        startDateEntry = dates[i - 1];
+                    else 
+                        startDateEntry = dates[i];
+                }
+
+                if (date >= endDate && endDateEntry == DateTime.MaxValue)
+                    endDateEntry = dates[i];
+            }
+
+            for (int i = 0; i < durationsPerDate.Count; i++)
             {
                 var date = dates[i];
                 if (date >= startDate && date <= endDate)
@@ -77,6 +94,11 @@
                     durationsPerDateInTimeRange.Add(date, durationsPerDate[date]);
                 }
             }
+            if (!durationsPerDateInTimeRange.ContainsKey(startDateEntry) && startDateEntry != DateTime.MinValue)
+                durationsPerDateInTimeRange.Add(startDate, durationsPerDate[startDateEntry]);
+
+            if (!durationsPerDateInTimeRange.ContainsKey(endDateEntry) && endDateEntry != DateTime.MaxValue)
+                durationsPerDateInTimeRange.Add(endDate, durationsPerDate[endDateEntry]);
 
             return durationsPerDateInTimeRange;
         }
@@ -90,9 +112,14 @@
 	        {
 		        foreach (var entry in task.GetTogglEntrySums())
 		        {
-			        durationsPerDate.Add(entry.Date, 0);                                //Start of the Day
-			        durationsPerDate.Add(entry.Date.AddHours(23.9), entry.Duration);    //End of the Day
-		        }
+			        if (!durationsPerDate.ContainsKey(entry.Date))
+                        durationsPerDate.Add(entry.Date, 0);                            //Start of the Day
+
+                    if (!durationsPerDate.ContainsKey(entry.Date.AddDays(1)))
+			            durationsPerDate.Add(entry.Date.AddDays(1), entry.Duration);    //End of the Day
+                    else
+                        durationsPerDate[entry.Date.AddDays(1)] += entry.Duration;
+                }
 	        }
 
 	        foreach (var entry in durationsPerDate.Keys.ToList())
