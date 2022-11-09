f5412d9f280c5e7f17a67135d871885e9daabf80
DuplosFidibuss <lukas.jans@swisscows.email>
2022-11-09 18:00:39 +0100

    Create TogglLoadOverviewData.cs

---
+++ AcademicTimePlanner/Data/TogglLoadOverviewData.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademicTimePlanner.Data
+{
+    public class TogglLoadOverviewData
+    {
+        public string? TogglProjectName { get; }
+
+        public bool IsDeleted { get; }
+
+        public string? PlanProjectName { get; }
+    }
+}
