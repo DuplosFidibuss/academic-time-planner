52dc21eacf846e9ed7aeb4309bdadbfec49bec6d
ferbemar <ferbemar@students.zhaw.ch>
2022-11-15 18:37:12 +0100

    Update TogglServiceTests.cs
    wrote test for response with tasks, without tasks, projects without entries and entries without project

--- AcademicTimePlanner.Tests/TogglServiceTests.cs
+++ AcademicTimePlanner.Tests/TogglServiceTests.cs
@@ -25,15 +25,6 @@ public class TogglServiceTests
         var taskName = "Test task";
         var projectIds = new List<long>() { 1, 2 };
         var taskIds = new List<long>() { 3, 4, 5 };
-
-
-        // Arrange
-        var dates = new List<DateTime>()
-        {
-            new (2022, 01, 01),
-            new (2022, 01, 02),
-            new (2022, 01, 03),
-        };
         
         var togglDetailResponse = new TogglDetailResponse()
         {
@@ -65,6 +56,192 @@ public class TogglServiceTests
         {
             Assert.IsTrue(expectedTogglProjectList[index].TogglId == actualTogglProjectList[index].TogglId);
             Assert.IsTrue(expectedTogglProjectList[index].Name == projectName);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestReceiveNormalDataWithoutTask()
+    {
+        var projectName = "Test project";
+        var projectIds = new List<long>() { 1, 2, 3 };
+
+
+        // Arrange
+        var dates = new List<DateTime>()
+        {
+            new (2022, 01, 01),
+            new (2022, 01, 02),
+            new (2022, 01, 03),
+        };
+
+        var togglDetailResponse = new TogglDetailResponse()
+        {
+            Data = new List<TogglDetailResponseData>()
+            {
+                new() { Id = 0, ProjectId = projectIds[0], Project = projectName },
+                new() { Id = 1, ProjectId = projectIds[1], Project = projectName },
+                new() { Id = 2, ProjectId = projectIds[2], Project = projectName },
+            }
+        };
+
+        var expectedTogglProjectList = new List<TogglProject>()
+        {
+            new (projectIds[0], projectName),
+            new (projectIds[1], projectName),
+            new (projectIds[2], projectName),
+        };
+
+        _togglApiServiceMock
+            .Setup(mock => mock.GetDetailsSinceAsync(It.IsAny<DateOnly>()))
+            .ReturnsAsync(togglDetailResponse);
+
+        // Act
+        var actualTogglProjectList = await _componentUnderTest.GetTogglProjects(It.IsAny<DateOnly>());
+
+        // Asset
+        Assert.AreEqual(expectedTogglProjectList.Count, actualTogglProjectList.Count);
+        for (var index = 0; index < expectedTogglProjectList.Count; index++)
+        {
+            Assert.IsTrue(expectedTogglProjectList[index].TogglId == actualTogglProjectList[index].TogglId);
+            Assert.IsTrue(expectedTogglProjectList[index].Name == projectName);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestReceiveNormalDataWithTask()
+    {
+        var projectName = "Test project";
+        var taskName = "Test task";
+        var projectIds = new List<long>() { 0, 1, 2 };
+        var taskIds = new List<long>() { 3, 4, 5 };
+
+        var togglDetailResponse = new TogglDetailResponse()
+        {
+            Data = new List<TogglDetailResponseData>()
+            {
+                new() { Id = 0, ProjectId = projectIds[0], TaskId = taskIds[0], Project = projectName, Task = taskName },
+                new() { Id = 1, ProjectId = projectIds[1], TaskId = taskIds[1], Project = projectName, Task = taskName },
+                new() { Id = 2, ProjectId = projectIds[2], TaskId = taskIds[2], Project = projectName, Task = taskName },
+            }
+        };
+
+        var expectedTogglProjectList = new List<TogglProject>()
+        {
+            new (projectIds[0], projectName),
+            new (projectIds[1], projectName),
+            new (projectIds[2], projectName),
+        };
+
+        int i = 0;
+        foreach (var togglProject in expectedTogglProjectList)
+        {
+            togglProject.AddTogglTask(taskIds[i], taskName);
+            i++;
+        }
+
+        _togglApiServiceMock
+            .Setup(mock => mock.GetDetailsSinceAsync(It.IsAny<DateOnly>()))
+            .ReturnsAsync(togglDetailResponse);
+
+        // Act
+        var actualTogglProjectList = await _componentUnderTest.GetTogglProjects(It.IsAny<DateOnly>());
+
+        // Asset
+        Assert.AreEqual(expectedTogglProjectList.Count, actualTogglProjectList.Count);
+        for (var index = 0; index < expectedTogglProjectList.Count; index++)
+        {
+            Assert.IsTrue(expectedTogglProjectList[index].TogglId == actualTogglProjectList[index].TogglId);
+            Assert.IsTrue(expectedTogglProjectList[index].Name == projectName);
+            Assert.IsTrue(expectedTogglProjectList[index].Tasks.Count == actualTogglProjectList[index].Tasks.Count);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestReceiveDataWithoutEntries()
+    {
+        var projectName = "Test project";
+        var projectIds = new List<long>() { 0, 1, 2 };
+
+        var togglDetailResponse = new TogglDetailResponse()
+        {
+            Data = new List<TogglDetailResponseData>()
+            {
+                new() { Id = 0, ProjectId = projectIds[0], Project = projectName },
+                new() { Id = 1, ProjectId = projectIds[1], Project = projectName },
+                new() { Id = 2, ProjectId = projectIds[2], Project = projectName },
+            }
+        };
+
+        var expectedTogglProjectList = new List<TogglProject>()
+        {
+            new (projectIds[0], projectName),
+            new (projectIds[1], projectName),
+            new (projectIds[2], projectName),
+        };
+
+        _togglApiServiceMock
+            .Setup(mock => mock.GetDetailsSinceAsync(It.IsAny<DateOnly>()))
+            .ReturnsAsync(togglDetailResponse);
+
+        // Act
+        var actualTogglProjectList = await _componentUnderTest.GetTogglProjects(It.IsAny<DateOnly>());
+
+        // Asset
+        Assert.AreEqual(expectedTogglProjectList.Count, actualTogglProjectList.Count);
+        for (var index = 0; index < expectedTogglProjectList.Count; index++)
+        {
+            Assert.IsTrue(expectedTogglProjectList[index].TogglId == actualTogglProjectList[index].TogglId);
+            Assert.IsTrue(expectedTogglProjectList[index].Name == projectName);
+            Assert.IsTrue(expectedTogglProjectList[index].GetTotalDuration() == 0);
+        }
+
+    }
+
+    [TestMethod]
+    public async Task TestReceiveEntriesDataWithoutProject()
+    {
+        // Arrange
+        var dates = new List<DateTime>()
+        {
+            new (2022, 01, 01),
+            new (2022, 01, 02),
+            new (2022, 01, 03),
+        };
+
+        var durations = new List<int>();
+        durations.Add(1);
+        durations.Add(2);
+        durations.Add(3);
+
+
+        var togglDetailResponse = new TogglDetailResponse()
+        {
+            Data = new List<TogglDetailResponseData>()
+            {
+                new() { Id = 0, StartTime = dates[0], EndTime = dates[1], Duration = durations[0] },
+                new() { Id = 1, StartTime = dates[0], EndTime = dates[2], Duration = durations[1] },
+                new() { Id = 2, StartTime = dates[1], EndTime = dates[2], Duration = durations[2] },
+            }
+        };
+
+        var expectedTogglProjectList = new List<TogglProject>()
+        {
+
+        };
+
+        _togglApiServiceMock
+            .Setup(mock => mock.GetDetailsSinceAsync(It.IsAny<DateOnly>()))
+            .ReturnsAsync(togglDetailResponse);
+
+        // Act
+        var actualTogglProjectList = await _componentUnderTest.GetTogglProjects(It.IsAny<DateOnly>());
+
+        // Asset
+        Assert.AreEqual(expectedTogglProjectList.Count, actualTogglProjectList.Count);
+        for (var index = 0; index < expectedTogglProjectList.Count; index++)
+        {
+            Assert.IsNull(actualTogglProjectList);
+            Assert.IsTrue(actualTogglProjectList[index].GetTotalDuration() == durations[index]);
         }
     }
 }
