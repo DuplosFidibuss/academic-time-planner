cf87da9d8a1b2e34378b2cb1c129079a7e1e9a6e
ferbemar <ferbemar@students.zhaw.ch>
2022-11-21 17:10:18 +0100

    Create PlanTask.cs

---
+++ AcademicTimePlanner/DataMapping/Plan/PlanTask.cs
@@ -0,0 +1,27 @@
+﻿namespace AcademicTimePlanner.DataMapping.Plan
+{
+    public class PlanTask
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public long TogglId { get; set; }
+
+        private PlanTask() { }
+
+        public PlanTask (Guid id, string name, long togglId)
+        {
+            Id = id;
+            Name = name;
+            TogglId = togglId;
+        }
+
+        public PlanTask(string name, long togglId)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            TogglId = togglId;
+        }
+    }
+}
